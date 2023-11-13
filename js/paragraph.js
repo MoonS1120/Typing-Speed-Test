dc702e507057e16c9b05bb8478ed5fd83ccf8688
MoonS <msuyoung5@gmail.com>
2023-11-13 17:21:15 +0800

    Add text paragarphs

---
+++ js/paragraph.js
@@ -0,0 +1,22 @@
+const paragraphs = [
+    "The sun dipped below the horizon, casting a warm glow across the tranquil meadow. Birds twittered their evening songs, and a gentle breeze whispered through the tall grass. Nature's symphony played as day gave way to night.",
+    "In the heart of the bustling city, neon lights painted the streets in vibrant hues. A sea of people moved in orchestrated chaos, each with a purpose known only to them. Among the skyscrapers, dreams and ambitions reached for the heavens.",
+    "Deep in the enchanted forest, ancient trees stood as silent guardians. Moss-covered stones marked the passage of time, and magical creatures flitted between shadows. The air hummed with mystical energy, inviting those who dared to explore.",
+    "Waves crashed against the rugged cliffs, sending salty spray into the air. Seagulls circled overhead, their cries echoing the rhythm of the ocean. A lone lighthouse stood tall, guiding ships through the tempest.",
+    "Amidst the vast desert, shifting sands painted a landscape that seemed to stretch into infinity. The scorching sun painted the dunes in shades of gold and orange, creating an otherworldly panorama.",
+    "In a quaint village, cobblestone streets wound their way between charming cottages. The aroma of freshly baked bread wafted from a local bakery, and laughter echoed in the air as children played in the town square.",
+    "High in the mountains, a crystal-clear lake mirrored the towering peaks that surrounded it. Pine trees stood tall, their needles rustling in the mountain breeze. The air was crisp, and the only sounds were the calls of distant wildlife.",
+    "A futuristic cityscape glittered with the neon glow of holographic billboards. Hovercraft zipped through the air, and pedestrians moved with purpose on elevated walkways. The city pulsed with the energy of innovation.",
+    "On a tropical island, palm trees swayed in the gentle breeze. White sand beaches met the turquoise waters of the ocean, inviting relaxation and escape. Colorful fish darted through coral reefs beneath the surface.",
+    "Fields of wildflowers stretched towards the horizon, a riot of colors that danced in the sunlight. Bees hummed busily among the blossoms, and butterflies floated on the breeze. It was a scene of natural beauty and harmony.",
+    "A mysterious castle perched atop a craggy hill, shrouded in mist. Turrets reached towards the sky, and ivy clung to ancient stones. Whispers of forgotten tales lingered in the air.",
+    "The hum of a bustling market filled the air as vendors peddled their wares. Exotic spices mingled with the scent of fresh fruit, creating a sensory feast for those navigating the crowded aisles.",
+    "The moon cast a silvery glow over a sleepy town, its streets bathed in the soft light. Stars twinkled in the night sky, and a sense of tranquility settled over the landscape.",
+    "A train rumbled through a vast, open landscape, its horn echoing in the stillness. Fields of crops stretched to the horizon, and a distant mountain range framed the scene.",
+    "In a snow-covered village, quaint cottages nestled under a blanket of white. Smoke curled from chimneys, and the air was filled with the sound of laughter as families gathered around crackling fires.",
+    "A technology-filled laboratory buzzed with activity as scientists worked diligently on cutting-edge experiments. Screens displayed intricate data, and the air hummed with the potential for groundbreaking discoveries.",
+    "A rustic cabin stood alone in a dense forest, surrounded by towering trees. A babbling brook meandered nearby, and the only sounds were the rustling of leaves and the occasional call of a distant owl.",
+    "Amid the ruins of an ancient civilization, time-worn stones told stories of a bygone era. Vines crawled over weathered structures, and a sense of history hung in the air.",
+    "A crowded concert venue pulsed with the beat of music as the crowd swayed in unison. Spotlights illuminated the stage, and the energy of the performance reverberated through the excited audience.",
+    "A coastal cliff overlooked a stormy sea, waves crashing against rocks with unrestrained force. Seabirds soared on the gusts of wind, and the scent of salt filled the air. It was a scene of raw, untamed beauty."
+];
