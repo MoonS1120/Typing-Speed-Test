9fe334f1d803cc8cc04662ce7f519da1ff37da6a
MoonS <msuyoung5@gmail.com>
2024-02-18 23:12:35 +0800

    Add typing detail (colour code)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const typingText = document.querySelector(".typing-text p"),
-inpField = document.querySelector(".content-box .input-field");
+inputField = document.querySelector(".content-box .input-field");
+
+charIndex = 0;
 
 function randomParagraph() {
     let randomIndex = Math.floor(Math.random()*paragraphs.length)
@@ -10,8 +12,28 @@ function randomParagraph() {
     })
     typingText.querySelectorAll("span")[0].classList.add("active");
 
-    document.addEventListener("keydown", () => inpField.focus());
-    typingText.addEventListener("click", () => inpField.focus());
+    document.addEventListener("keydown", () => inputField.focus());
+    typingText.addEventListener("click", () => inputField.focus());
+}
+
+function initTyping() {
+    const characters = typingText.querySelectorAll("span");
+    let typedChar = inputField.value.split("")[charIndex];
+
+    if (typedChar == null) {
+        charIndex--;
+        characters[charIndex].classList.remove("correct", "incorrect");
+    } else {
+        if(characters[charIndex].innerText===typedChar) {
+            characters[charIndex].classList.add("correct");
+        } else {
+            characters[charIndex].classList.add("incorrect");
+        }
+        charIndex++;
+    }
+    characters.forEach(span => span.classList.remove("active"))
+    characters[charIndex].classList.add("active");
 }
 
 randomParagraph();
+inputField.addEventListener("input", initTyping);
